b313fae9ac40fe61da25d1738c576e941b7acc98
allenkay76 <allenkay76@protonmail.com>
2023-04-02 07:15:25 +1000

    Removing the included headers as this is only a compile time thing that should be ignored

--- include/GameEngine/IScript.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class IScript {
-public:
-    virtual void Awake() = 0;
-    virtual void Start() = 0;
-    virtual void Update() = 0;
-
-    virtual ~IScript() {}
-};
--- include/SDL/SDLTexSurfData.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <memory>
-#include <SDL2/SDL.h>
-using namespace std;
-struct SDLTexSurfData {
-    unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> texturePtr{nullptr, &SDL_DestroyTexture};
-    unique_ptr<SDL_Surface, decltype(&SDL_FreeSurface)> surfacePtr{nullptr, &SDL_FreeSurface};
-};
